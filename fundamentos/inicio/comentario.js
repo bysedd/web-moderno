7ece4190ed7ebbee1593f505e0b321b2be88751e
Felippe <4drade@gmail.com>
2023-04-10 14:06:48 -0300

    comentários

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,11 @@
+// console.log é usado para imprimir uma linha em branco no console.
+console.log('Passo #03');
+console.log('Passo #01'); // mais um exemplo de console.log
+console.log('Passo #02');
+
+/*
+ * Este é um
+ * comentário
+ * de múltiplas
+ * linhas
+ */
