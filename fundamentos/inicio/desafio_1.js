d58bcb365d0fafb2f2cc4bdfd0d1c83874734136
Felippe <4drade@gmail.com>
2023-04-10 21:01:39 -0300

    desafio 1

---
+++ fundamentos/inicio/desafio_1.js
@@ -0,0 +1,36 @@
+/*
+    Exibir no console os números pares e ímpares de 1 a 10
+    
+    // Bloco 1
+    1
+    3
+    5
+    7
+    9
+    
+    // Bloco 2
+    2
+    4
+    6
+    8
+    10
+ */
+
+{
+    // Bloco 1
+    for (let i = 1; i <= 10; i++) {
+        if (i % 2 !== 0) {
+            console.log(i)
+        }
+    }
+    console.log()
+}
+
+{
+    // Bloco 2
+    for (let i = 1; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+    }
+}
