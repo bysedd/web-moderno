b185514f7d54b5807114020902456712aab968db
Felippe <4drade@gmail.com>
2023-04-11 18:00:55 -0300

    trabalhando com dados #1

---
+++ fundamentos/inicio/dados_1.js
@@ -0,0 +1,11 @@
+let nome = "Caneta";
+let quantidade = 10;
+let price = 6.4;
+let imposto = 1.5;
+let finalPrice = price + imposto;
+
+console.log("Produto: " + nome);
+console.log("Quantidade: " + quantidade);
+console.log("Preço: " + price);
+console.log("Imposto: " + imposto);
+console.log("Preço Final: " + finalPrice);
