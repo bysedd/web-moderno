0e0905165b320244d1f436cfb826a49f90eed134
Felippe <4drade@gmail.com>
2023-04-12 08:18:26 -0300

    refatorando

--- fundamentos/inicio/dados_1.js
+++ fundamentos/inicio/dados_1.js
@@ -4,8 +4,8 @@ let price = 6.4;
 let imposto = 1.5;
 let finalPrice = price + imposto;
 
-console.log("Produto: " + nome);
-console.log("Quantidade: " + quantidade);
-console.log("Preço: " + price);
-console.log("Imposto: " + imposto);
-console.log("Preço Final: " + finalPrice);
+console.log(`Produto: ${nome}`);
+console.log(`Quantidade: ${quantidade}`);
+console.log(`Preço: ${price}`);
+console.log(`Imposto: ${imposto}`);
+console.log(`Preço Final: ${finalPrice}`);
