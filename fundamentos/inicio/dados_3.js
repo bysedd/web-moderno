cc8fb4dc82855f1af2d07b8dc9f62ec67ee20c2d
Felippe <4drade@gmail.com>
2023-04-12 17:42:04 -0300

    trabalhando com dados #3

---
+++ fundamentos/inicio/dados_3.js
@@ -0,0 +1,11 @@
+let age = 17;
+console.log(typeof age);
+
+let salary = 4578.32;
+console.log(typeof salary);
+
+let isRaining = true;
+console.log(typeof isRaining);
+
+let name = "Felippe";
+console.log(typeof name);
