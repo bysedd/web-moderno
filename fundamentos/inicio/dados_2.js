e7e999e8cd5dcf54cb78ab0d5f0f2c1dafe10108
Felippe <4drade@gmail.com>
2023-04-12 17:35:04 -0300

    trabalhando com dados #2

---
+++ fundamentos/inicio/dados_2.js
@@ -0,0 +1,16 @@
+let price = 19.90;
+let discount = 0.4;
+// console.log(`Discount price: ${price * (1 - discount)}`);
+
+let finalPrice = price * (1 - discount);
+// console.log(`Discount price: ${finalPrice.toFixed(2)}`);
+
+let product = {
+    "name": "Pencil",
+    "category": "Stationery",
+    "price": price,
+    "discount": discount,
+}
+for (const k in product) {
+    console.log(`${k}: ${product[k]}`);
+}
