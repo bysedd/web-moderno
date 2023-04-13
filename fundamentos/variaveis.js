ce4bae5831f32681d29bd827c906bd3550f2e378
Felippe <4drade@gmail.com>
2023-04-13 10:43:55 -0300

    variáveis e constantes

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,17 @@
+// Variáveis e Constantes
+
+var a = 3;
+let b = 4;
+console.log(a, b);
+
+var a = 30;
+b = 40; // 'let' não permite redeclarar a variável
+console.log(a, b);
+
+a = 300;
+b = 400;
+console.log(a, b);
+
+const c = 5;
+// c = 50;
+console.log(c);
