d616b4d44325a598fed6fece2a665ea53c6cc622
Felippe <4drade@gmail.com>
2023-04-13 20:15:16 -0300

    objeto.js

---
+++ fundamentos/objeto.js
@@ -0,0 +1,11 @@
+const prod1 = {};
+prod1.name = "Redmi Note 12";
+prod1.price = 1224.59;
+prod1["Discount"] = 0.40; // Avoid attributes with space
+console.log(prod1);
+
+const prod2 = {
+    name: "T-shirt",
+    price: 79.90,
+}
+console.log(prod2);
