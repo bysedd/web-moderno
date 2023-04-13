d640e387af8afe29da37f01b62e6bd730c4d1ad6
Felippe <4drade@gmail.com>
2023-04-13 16:38:37 -0300

    template string

---
+++ fundamentos/template_string.js
@@ -0,0 +1,15 @@
+let name = "Rebeca";
+const template = `
+    Hi
+    ${name}!
+`;
+console.log(template);
+
+// expressions...
+console.log(`1 + 1 = ${1 + 1}`);
+
+// arrow function...
+const print = text => console.log(text);
+const upper = text => text.toUpperCase();
+
+print(`Hey... ${upper("cuidado")}!`);
