8aa0be7ec715eccd375a7ee939a71eab81ed1d2c
Felippe <4drade@gmail.com>
2023-04-15 15:22:54 -0300

    using var in loop

---
+++ fundamentos/var_loop_2.js
@@ -0,0 +1,13 @@
+const func = [];
+
+for (var i = 0; i < 10; i++) {
+    func.push(() => console.log(i));
+}
+
+func[2]();
+func[8]();
+
+// The result is 10, 10, because the variable 'i' is global and the function
+// func[2] and func[8] are closures that refer to the global variable 'i'.
+// The variable 'i' is incremented until value 10, and the functions
+// func[2] and func[8] are closures that refer to the global variable 'i'.
