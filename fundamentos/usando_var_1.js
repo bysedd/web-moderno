d8d357eef111b8e63dd2aee80eeac50e01bd08d2
Felippe <4drade@gmail.com>
2023-04-14 23:09:45 -0300

    usando var #1

---
+++ fundamentos/usando_var_1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = "Será???"; // var é global
+                console.log(sera);
+            }
+        }
+    }
+}
+console.log(sera);
+
+function test() {
+    var local = 123;
+    console.log(local);
+}
+
+test();
+// console.log(local); // ReferenceError: local is not defined
