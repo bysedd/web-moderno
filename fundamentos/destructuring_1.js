089717459afbd7621deaae4467d9621e13d11855
Felippe <4drade@gmail.com>
2023-04-19 20:55:18 -0300

    operadores: destructuring

---
+++ fundamentos/destructuring_1.js
@@ -0,0 +1,23 @@
+const people = {
+    "name": "Ana",
+    "age": 5,
+    "address": {
+        "street": "Rua ABC",
+        "number": 1000
+    }
+};
+
+const { name, age } = people;
+console.log(name, age);
+
+const { name: n, age: a } = people;
+console.log(n, a);
+
+const { lastName, goodHumored = true } = people;
+console.log(lastName, goodHumored);
+
+const { address: { street, number, cep } } = people;
+console.log(street, number, cep);
+
+// const { account: { ag, num } } = people;
+// console.log(ag, num);
