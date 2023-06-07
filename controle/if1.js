aca98be0f3603daebce50ac95141ed8d8e30969b
Felippe <4drade@gmail.com>
2023-06-07 16:20:10 -0300

    usando a estrutura IF

---
+++ controle/if1.js
@@ -0,0 +1,28 @@
+function sohBoaNoticia(nota) {
+    if (nota >= 7) {
+        console.log('Aprovado com nota: ' + nota);
+    }
+}
+
+sohBoaNoticia(8.1);
+sohBoaNoticia(6.1);
+
+function seForVerdadeEuFalo(valor) {
+    if (valor) {
+        console.log(`É verdade... "${valor}"`);
+    }
+}
+
+seForVerdadeEuFalo(); // false
+seForVerdadeEuFalo(null); // false
+seForVerdadeEuFalo(undefined); // false
+seForVerdadeEuFalo(NaN); // false
+seForVerdadeEuFalo(''); // false
+seForVerdadeEuFalo(0); // false
+
+seForVerdadeEuFalo(-1); // true
+seForVerdadeEuFalo(' '); // true
+seForVerdadeEuFalo('?'); // true
+seForVerdadeEuFalo([]); // true
+seForVerdadeEuFalo([1, 2]); // true
+seForVerdadeEuFalo({}); // true
